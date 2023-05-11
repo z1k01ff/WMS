be669f4beb9a1f8b2b2de82a015d1444b3ee53b5
z1k01ff <z1k01ff@gmail.com>
2023-05-12 00:01:46 +0300

    time and data (headless mode)

--- AIO_bot.py
+++ AIO_bot.py
@@ -3,7 +3,7 @@ import json
 from aiogram import Bot, Dispatcher, executor, types
 # from aiogram.utils.markdown import hbold, hunderline, hcode, hlink
 from aiogram.dispatcher.filters import Text
-from test import update_time
+from main import update_time, update_date
 
 TOKEN = "5662776987:AAFNQiftIFBgayordIizZxMeRDcZWCmq7Ao"
 
@@ -13,7 +13,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    start_buttons = ["Time"]
+    start_buttons = ["Time", "Date", "Поповнення", "Перепаковка"]
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*start_buttons)
 
@@ -21,12 +21,21 @@ async def start(message: types.Message):
 
 
 @dp.message_handler(Text(equals="Time"))
-async def get_all_news(message: types.Message):
+async def get_time(message: types.Message):
     update_time()
     with open("time.json") as file:
         current_time = json.load(file)
 
     await message.answer(current_time)
+
+
+@dp.message_handler(Text(equals="Date"))
+async def get_date(message: types.Message):
+    update_date()
+    with open("date.json") as file:
+        current_date = json.load(file)
+
+    await message.answer(current_date)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
--- main.py
+++ main.py
@@ -1,27 +1,31 @@
+import time
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import json
 from selenium.webdriver.chrome.options import Options
 
+url = "https://time.is/ru/"
+chrome_path = '/Users/admin/Downloads/chromedriver'
+s = Service(executable_path=chrome_path)
+
+# Прихований режим
+options = Options()
+options.add_argument("--headless=new")
 
 
 def update_time() -> object:
-    driver_path = '/Users/admin/Downloads/chromedriver'  # mac driver
-    s = Service(executable_path=driver_path)
-    driver = webdriver.Chrome(service=s)
-    options = Options()
-    options.add_argument('--disable-blink-features=AutomationControlled')
+    driver = webdriver.Chrome(service=s, options=options)
 
     global time1
     try:
-        url = "https://time.is/ru/"
         driver.maximize_window()
+        driver.get(url)
         time1 = driver.find_element(By.ID, "clock").text
 
-        # time.sleep(5)
 
     except Exception as ex:
         print(ex)
@@ -38,13 +42,25 @@ def update_time() -> object:
     return time1
 
 
-def main():
-    pass
-    # print(update_time())
-    # print(update_time())
+def update_date() -> object:
+    driver = webdriver.Chrome(service=s)
+    global date1
+    try:
+        driver.maximize_window()
+        driver.get(url)
+        date1 = driver.find_element(By.ID, "dd").text
 
 
-# if __name__ == '__main__':
-#     pass
+    except Exception as ex:
+        print(ex)
 
+    finally:
+        with open("date.json", "w") as file:
+            json.dump(date1, file, indent=4, ensure_ascii=False)
 
+        print(date1)
+
+        driver.close()
+        driver.quit()
+
+    return date1
